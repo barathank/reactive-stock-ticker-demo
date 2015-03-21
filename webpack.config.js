456188e92410026b69dfcb106e81a0d2f3e24d1d
Ben Anderson <banderson@constantcontact.com>
2015-03-21 00:54:07 -0400

    Added react-hot-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,25 @@
+var webpack = require('webpack');
+
 module.exports = {
-  entry: './js/index.js',
+  entry: [
+    'webpack-dev-server/client?http://localhost:3000',
+    'webpack/hot/only-dev-server',
+    './js/index.js'
+  ],
   output: {
-    path: './js/',
+    path: __dirname + '/js/',
     publicPath: '/js/',
-    filename: 'bundle.js'
+    filename: 'bundle.js',
+    hot: true
   },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ],
   module: {
     loaders: [
-      { test: /\.(jsx|js)$/, exclude: /node_modules/, loader: 'babel-loader?experimental'},
+      { test: /\.jsx?$/, loaders: ['react-hot', 'jsx?harmony'], exclude: /node_modules/ },
+      { test: /\.(jsx|js)$/, loader: 'babel-loader?experimental', exclude: /node_modules/ },
       { test: /\.css$/, loader: 'style-loader!css-loader' }
       // { test: /\.(png|jpg)$/, loader: 'url-loader?limit=8192' } // inline base64 URLs for <=8k images, direct URLs for the rest
     ]
