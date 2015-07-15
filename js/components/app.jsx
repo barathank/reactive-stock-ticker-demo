202514b9375da593499411cff8d2990ce2e0519e
Ben Anderson <banderson@hubspot.com>
2015-07-14 20:59:34 -0400

    Dev tools!!!!!!!!!!!!!!!!!!!!

--- js/components/app.jsx
+++ js/components/app.jsx
@@ -1,17 +1,35 @@
 import React from 'react';
 import TickerApp from './TickerApp';
-import {createStore, combineReducers} from 'redux';
+import {createStore, combineReducers, applyMiddleware, compose} from 'redux';
 import {Provider} from 'react-redux';
 import * as reducers from '../reducers/index';
 
-const store = createStore(combineReducers(reducers));
+// Devtools
+import thunk from 'redux-thunk';
+import { devTools, persistState } from 'redux-devtools';
+import { DevTools, DebugPanel, LogMonitor } from 'redux-devtools/lib/react';
+
+const finalCreateStore = compose(
+  applyMiddleware(thunk),
+  devTools(),
+  persistState(window.location.href.match(/[?&]debug_session=([^&]+)\b/)),
+  createStore
+);
+
+const store = finalCreateStore(combineReducers(reducers));
 
 export default React.createClass({
   render() {
     return (
-      <Provider store={store}>
-        {() => <TickerApp /> }
-      </Provider>
+      <div>
+        <Provider store={store}>
+          {() => <TickerApp /> }
+        </Provider>
+        <DebugPanel top right bottom>
+          <DevTools store={store}
+                    monitor={LogMonitor} />
+        </DebugPanel>
+      </div>
     );
   }
 });
