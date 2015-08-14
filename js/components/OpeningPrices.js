0dba91c7438c1e5ce3b538a73f192976767aa76c
Ben Anderson <banderson@hubspot.com>
2015-08-13 22:56:36 -0400

    More renaming and cleanup

--- js/components/OpeningPrices.js
+++ js/components/OpeningPrices.js
@@ -2,25 +2,25 @@ import React from 'react';
 import OpeningPricesFilter from '../data/filters/OpeningPrices';
 import {connect} from 'react-redux';
 
-export default connect(OpeningPricesFilter)(
-  class InitialPrices {
-    static propTypes = {
-      prices: React.PropTypes.array.isRequired
-    }
-
-    static defaultProps = {
-      prices: []
-    }
-
-    render() {
-      const {prices} = this.props;
-      return (
-        <ul>
-          {prices.map(({key, openPrice}) =>
-            <li key={key}>{key}: {openPrice}</li>
-          )}
-        </ul>
-      );
-    }
+class OpeningPrices {
+  static propTypes = {
+    prices: React.PropTypes.array.isRequired
   }
-)
+
+  static defaultProps = {
+    prices: []
+  }
+
+  render() {
+    const {prices} = this.props;
+    return (
+      <ul>
+        {prices.map(({key, openPrice}) =>
+          <li key={key}>{key}: {openPrice}</li>
+        )}
+      </ul>
+    );
+  }
+}
+
+export default connect(OpeningPricesFilter)(OpeningPrices);
--- js/components/TickerApp.js
+++ js/components/TickerApp.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import Ticker from './Ticker';
-import InitialPrices from './InitialPrices';
-import styles from '../../css/styles.css';
+import OpeningPrices from './OpeningPrices';
 import * as Actions from '../actions/TickerActions';
+import styles from '../../css/styles.css';
 
-import { bindActionCreators } from 'redux';
-import { connect } from 'react-redux';
+import {bindActionCreators} from 'redux';
+import {connect} from 'react-redux';
 
 import {default as VisiblePrices} from '../data/filters/VisiblePrices';
 
@@ -19,13 +19,10 @@ class TickerApp extends React.Component {
           onStart={actions.startTicker.bind(null, actions.receiveData)}
           onStop={actions.stopTicker} onReset={actions.reset}
         />
-        <InitialPrices stocks={stocks} />
+        <OpeningPrices />
       </div>
     );
   }
 }
 
-TickerApp = connect(VisiblePrices)(TickerApp);
-TickerApp = connect(state => ({stocks: state.Stocks}))(TickerApp);
-
-export default TickerApp;
+export default connect(VisiblePrices)(TickerApp);
